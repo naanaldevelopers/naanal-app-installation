3e0f496d48e5f4f4f02d2789d33ce1593b6fc051
sankarn <sankarn@naanal.in>
2020-05-08 09:47:28 +0000

    adding-things

---
+++ config-script-templates/gunicorn/run-shipper.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+NAME="shipper"
+DJANGODIR=%APP_DIRECTORY%
+SOCKFILE=%APP_DIRECTORY%/Misc/shipper.sock
+USER=%USER%
+GROUP=%USER_GROUP%
+NUM_WORKERS=3
+DJANGO_SETTINGS_MODULE=shipper.settings
+DJANGO_WSGI_MODULE=shipper.wsgi
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+source $DJANGODIR/venv/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec $DJANGODIR/venv/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --bind=unix:$SOCKFILE \
+  --log-level=info \
+  --access-logfile='/var/log/shipper/access.log' \
+  --access-logformat '%({X-Forwarded-For}i)s %({X-Remote-User-Name}o)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' \
+  --timeout 900
---
+++ portal.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USER=$USER
+USER_GROUP=$USER
+USER_HOME=$(eval echo "~$USER")
+
+source helpers.env
+
+cd $USER_HOME
+echo "Please wait we're configuring our app for you."
+#Cloning repository.
+git clone -q https://$GIT_ACCESS_NAME:$GIT_ACCESS_TOKEN@gitlab.com/naanal/scm/portal.git 1>/dev/null
+cd portal
+APP_DIRECTORY=$(eval pwd)
+echo "Installing system dependencies..."
+#Installation of needed system packages for an app.
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - 1>/dev/null
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - 1>/dev/null
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list 1>/dev/null
+sudo apt-get update 1>/dev/null
+sudo apt-get install -y nodejs 1>/dev/null
+sudo apt-get install -y yarn 1>/dev/null
+sudo npm install -g firebase-tools > /dev/null 2>&1
+echo "Installation of system dependencies was done."
+echo "Installing app dependencies..."
+#Instllation of an app dependencies.
+yarn install --silent > /dev/null 2>&1
+echo "Installation of app dependencies was done."
+#Creating .env file with appropriate details.
+echo VUE_APP_API=https://%API_DOMAIN% >> .env
+sed -i "s|%API_DOMAIN%|$API_DOMAIN|"g $APP_DIRECTORY/.env
+#Building an app for production
+echo "Building an app for production"
+export NODE_OPTIONS=--max-old-space-size=4096
+yarn run build
+echo "Production build success"
+#Hosting project in firebase.
+firebase projects:create $FIREBASE_PROJECT
+firebase use $FIREBASE_PROJECT
+firebase deploy
+echo "Visit Hosting URL to reach our portal app."
