f162e5673b0eccecf3349ef8830d0f2a9922ebfd
naanaldevelopers <64519577+naanaldevelopers@users.noreply.github.com>
2020-05-11 16:10:48 +0530

    Update portal_firebase_deploy.sh

--- config-script-templates/feature-deployment/portal_firebase_deploy.sh
+++ config-script-templates/feature-deployment/portal_firebase_deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 APP_DIRECTORY=%APP_DIRECTORY%
 cd $APP_DIRECTORY
-source .env
-git pull https://$GIT_ACCESS_NAME:$GIT_ACCESS_TOKEN@gitlab.com/naanal/scm/portal.git
+git pull https://%GIT_ACCESS_NAME%:%GIT_ACCESS_TOKEN%@gitlab.com/naanal/scm/portal.git
 yarn install
 export NODE_OPTIONS=--max-old-space-size=4096
 yarn run build
